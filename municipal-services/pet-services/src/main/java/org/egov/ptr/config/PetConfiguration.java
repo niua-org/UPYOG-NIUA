1507d44972c327c76cbd6fa4b1e1935dfaf590fb
Kunal <kunalgaurav1420@gmail.com>
2025-09-10 16:38:52 +0530

    updated pom file for shedlock dependency , added postman collection

--- municipal-services/pet-services/src/main/java/org/egov/ptr/config/PetConfiguration.java
+++ municipal-services/pet-services/src/main/java/org/egov/ptr/config/PetConfiguration.java
@@ -3,7 +3,11 @@ package org.egov.ptr.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.util.TimeZone;
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +15,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
-
-
 
 @Import({ TracerConfiguration.class })
 @Getter
