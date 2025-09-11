305918facbb19e479a3eb36117644cf303898be7
ShivankShuklaa <Shivank@niua.org>
2025-09-11 14:48:54 +0530

    tree pruning service updated to LTS

--- municipal-services/tp-services/src/main/java/org/upyog/tp/web/models/Workflow.java
+++ municipal-services/tp-services/src/main/java/org/upyog/tp/web/models/Workflow.java
@@ -3,14 +3,14 @@ package org.upyog.tp.web.models;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.validation.annotation.Validated;
 import org.upyog.tp.web.models.workflow.Document;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.swagger.annotations.ApiModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,9 +21,9 @@ import lombok.Setter;
  * Represents a workflow object used for tracking actions, status, and related details
  * in the Tree Pruning booking process.
  */
-@ApiModel(description = "Represents a workflow object used for tracking actions, status, and related details in the Tree Pruning booking process.")
+@Schema(description = "Represents a workflow object used for tracking actions, status, and related details in the Tree Pruning booking process.")
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:52:32.717Z[GMT]")
+@jakarta.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-06-23T05:52:32.717Z[GMT]")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
--- municipal-services/tp-services/src/main/java/org/upyog/tp/web/models/treePruning/TreePruningBookingDetail.java
+++ municipal-services/tp-services/src/main/java/org/upyog/tp/web/models/treePruning/TreePruningBookingDetail.java
@@ -1,13 +1,13 @@
 package org.upyog.tp.web.models.treePruning;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.validation.annotation.Validated;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.swagger.annotations.ApiModel;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,9 +22,9 @@ import java.util.List;
 /**
  * Details for new booking of Tree Pruning
  */
-@ApiModel(description = "Details for new booking of Tree Pruning")
+@Schema(description = "Details for new booking of Tree Pruning")
 @Validated
-@javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-19T11:17:29.419+05:30")
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-19T11:17:29.419+05:30")
 @Getter
 @Setter
 @AllArgsConstructor
