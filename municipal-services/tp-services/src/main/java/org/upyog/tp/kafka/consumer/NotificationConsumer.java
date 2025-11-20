602c46e24c9a2f7376788a727cab009a879a6064
Kunal <kunalgaurav1420@gmail.com>
2025-11-20 14:27:04 +0530

    updated NotificationConsumer file for kafka issue

--- municipal-services/tp-services/src/main/java/org/upyog/tp/kafka/consumer/NotificationConsumer.java
+++ municipal-services/tp-services/src/main/java/org/upyog/tp/kafka/consumer/NotificationConsumer.java
@@ -36,9 +36,13 @@ public class NotificationConsumer {
      * @param topic  the name of the Kafka topic from which the message was received
      */
 
+
     @KafkaListener(topics = {
-            "${persister.update.tree-pruning.topic}", "${persister.create.tree-pruning.topic}, ${persister.create.tree-pruning.with.profile.topic}"
+            "${persister.update.tree-pruning.topic}",
+            "${persister.create.tree-pruning.topic}",
+            "${persister.create.tree-pruning.with.profile.topic}"
     })
+
     public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         Object request = null;
         String applicationStatus = null;
