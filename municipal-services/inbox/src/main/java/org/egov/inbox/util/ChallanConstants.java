03d939a3ea580553ebcc29df6517383c5459f2dd
Nikhil5922 <nikhilkumar7800@gmail.com>
2026-05-22 12:53:46 +0530

    Refactor constants by introducing CommonConstants and extending existing classes

--- municipal-services/inbox/src/main/java/org/egov/inbox/util/ChallanConstants.java
+++ municipal-services/inbox/src/main/java/org/egov/inbox/util/ChallanConstants.java
@@ -1,8 +1,7 @@
 package org.egov.inbox.util;
 
-public class ChallanConstants {
+public class ChallanConstants extends CommonConstants {
+
     public static final String CHALLAN_GENERATION = "Challan_Generation";
-    public static final String LOCALITY_PARAM = "locality";
-    public static final String STATUS_PARAM = "status";
-    public static final String OFFSET_PARAM = "offset";
+
 }
