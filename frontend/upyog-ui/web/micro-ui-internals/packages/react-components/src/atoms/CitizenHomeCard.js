4035e82e11e419e9c0ee4ccccff4f2ac06e1503e
HarshitaPurohit-11 <purohitharshita006@gmail.com>
2026-05-27 16:32:26 +0530

    fix: resolve key prop warnings across components

--- frontend/upyog-ui/web/micro-ui-internals/packages/react-components/src/atoms/CitizenHomeCard.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/react-components/src/atoms/CitizenHomeCard.js
@@ -21,19 +21,20 @@ const CitizenHomeCard = ({ header, links = [], state, Icon, Info, isInfo = false
       </div>
 
       <div className="links">
-        {updatedData.map((e, i) => (
-          <div key={i} className="linksWrapper" style={{paddingLeft:"10px"}}>
-            {(e?.parentModule?.toUpperCase() == "BIRTH" ||
-              e?.parentModule?.toUpperCase() == "DEATH" ||
-              e?.parentModule?.toUpperCase() == "FIRENOC") ?
-              <a href={e.link}>{e.i18nKey}</a> :
-              <Link key={i} to={{ pathname: e.link, state: e.state }}>
-                {e.i18nKey}
-              </Link>
-            }
-          </div>
-        ))}
-      </div>
+  {updatedData.map((e, i) => (
+    <div key={e?.id || i} className="linksWrapper" style={{ paddingLeft: "10px" }}>
+      {(e?.parentModule?.toUpperCase() == "BIRTH" ||
+        e?.parentModule?.toUpperCase() == "DEATH" ||
+        e?.parentModule?.toUpperCase() == "FIRENOC") ? (
+        <a href={e.link}>{e.i18nKey}</a>
+      ) : (
+        <Link to={{ pathname: e.link, state: e.state }}>
+          {e.i18nKey}
+        </Link>
+      )}
+    </div>
+  ))}
+</div>
       <div>{isInfo ? <Info /> : null}</div>
     </div>
   );
