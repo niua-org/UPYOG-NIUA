e0d73828d6c861da738637bc2498d7506ca6a4c3
HarshitaPurohit-11 <purohitharshita006@gmail.com>
2026-05-29 11:10:56 +0530

    Revert "fix: handle undefined locality query edge case"
    This reverts commit 63355bbad819419b8e3d7e8b1ba289cc1e6355aa.

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/getLocalities.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/getLocalities.js
@@ -4,17 +4,14 @@ import { StoreService } from "./Store/service";
 export const getLocalities = {
   admin: async (tenant) => {
     await StoreService.defaultData(tenant, tenant, Digit.StoreData.getCurrentLanguage());
-    const response = await LocationService.getLocalities(tenant);
-    return response?.TenantBoundary?.[0] || { boundary: [] };
+    return (await LocationService.getLocalities(tenant)).TenantBoundary[0];
   },
   revenue: async (tenant) => {
     await StoreService.defaultData(tenant, tenant, Digit.StoreData.getCurrentLanguage());
-    const response = await LocationService.getRevenueLocalities(tenant);
-    return response?.TenantBoundary?.[0] || { boundary: [] };
+    return (await LocationService.getRevenueLocalities(tenant)).TenantBoundary[0];
   },
   grampanchayats: async (tenant) => {
     await StoreService.defaultData(tenant, tenant, Digit.StoreData.getCurrentLanguage());
-    const response = await LocationService.getGramPanchayats(tenant);
-    return response?.TenantBoundary?.[0] || { boundary: [] };
+    return (await LocationService.getGramPanchayats(tenant)).TenantBoundary[0];
   },
 };
