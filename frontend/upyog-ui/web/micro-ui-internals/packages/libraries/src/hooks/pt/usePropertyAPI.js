208d21a1fe8ab49ecca778323b07eef702fac830
Srirranjan Srivastava <sriranjan.srivastava@pwc.com>
2026-06-02 14:18:43 +0530

    added CommonPT loader async issue

--- frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/pt/usePropertyAPI.js
+++ frontend/upyog-ui/web/micro-ui-internals/packages/libraries/src/hooks/pt/usePropertyAPI.js
@@ -1,11 +1,11 @@
 import { PTService } from "../../services/elements/PT";
 import { mutationTemplate } from "../../common/mutationTemplate";
 
-const usePropertyAPI = (tenantId, type = true) => {
+const usePropertyAPI = (tenantId, type = true, config = {}) => {
   if (type) {
-    return mutationTemplate({ mutationFn: (data) => PTService.create(data, tenantId) });
+    return mutationTemplate({ mutationFn: (data) => PTService.create(data, tenantId), config });
   } else {
-    return mutationTemplate({ mutationFn: (data) => PTService.update(data, tenantId) });
+    return mutationTemplate({ mutationFn: (data) => PTService.update(data, tenantId), config });
   }
 };
 
