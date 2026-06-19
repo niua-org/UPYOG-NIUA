2891edc093f460c5e95381cb10dc4ced92d1bee2
atulniua <167168460+atulniua@users.noreply.github.com>
2026-06-19 15:04:46 +0530

    Update SchemaDefinitionController.java
    Signed-off-by: atulniua <167168460+atulniua@users.noreply.github.com>

--- core-services/mdms-v2/src/main/java/org/egov/infra/mdms/controller/SchemaDefinitionController.java
+++ core-services/mdms-v2/src/main/java/org/egov/infra/mdms/controller/SchemaDefinitionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import jakarta.validation.Valid;
 import java.util.List;
+import org.egov.infra.mdms.repository.SchemaDefinitionRepository;
 
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2023-05-30T09:26:57.838+05:30[Asia/Kolkata]")
 @Controller
@@ -23,10 +24,16 @@ public class SchemaDefinitionController {
 
     private SchemaDefinitionService schemaDefinitionService;
 
+    private SchemaDefinitionRepository schemaDefinitionRepository;
+
     @Autowired
-    public SchemaDefinitionController(SchemaDefinitionService schemaDefinitionService) {
-        this.schemaDefinitionService = schemaDefinitionService;
-    }
+    public SchemaDefinitionController(
+        SchemaDefinitionService schemaDefinitionService,
+        SchemaDefinitionRepository schemaDefinitionRepository) {
+
+    this.schemaDefinitionService = schemaDefinitionService;
+    this.schemaDefinitionRepository = schemaDefinitionRepository;
+}
 
     /**
      * Request handler for serving schema create requests.
